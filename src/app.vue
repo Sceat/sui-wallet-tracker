6788fafecfccd3dd93e7c101fa53d8ef02a58e8e
Sceat <fetch@sceat.xyz>
2023-05-11 00:20:10 +0800

    feat: implement webapp

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,60 @@
 <template lang="pug">
-router-view
+.main
+  .address(:class="{ valid: isValidSuiAddress(path) }") {{ path }}
+  .tokens
+    .token(v-for="token in tokens")
+      .name {{ token.name }}
+      .balance {{ token.value }}$
+  .value {{ balance }}$
 </template>
 
 <script setup>
-const name = 'app';
+import BN from 'bignumber.js';
+import { get_tokens } from './sui.js';
+import { ref, watchEffect, computed } from 'vue';
+import { useRoute } from 'vue-router';
+import { isValidSuiAddress } from '@mysten/sui.js';
+
+const tokens = ref('');
+const balance = ref('');
+const route = useRoute();
+const path = computed(() => route.path.slice(1));
+
+watchEffect(async () => {
+  if (isValidSuiAddress(path.value)) {
+    const amounts = await get_tokens(path.value);
+    const response = await fetch(
+      `https://api.dexscreener.com/latest/dex/tokens/${Object.keys(
+        amounts
+      ).join(',')}`
+    );
+    const { pairs } = await response.json();
+    const seen_addresses = new Set();
+    const unique_pairs = pairs
+      .filter(
+        ({ chainId, baseToken: { address } }) =>
+          chainId === 'sui' && Object.keys(amounts).includes(address)
+      )
+      .filter(({ baseToken: { address } }) => {
+        if (seen_addresses.has(address)) return false;
+        seen_addresses.add(address);
+        return true;
+      });
+
+    tokens.value = unique_pairs.map(
+      ({ baseToken: { address, symbol }, priceUsd }) => ({
+        address,
+        name: symbol,
+        value: new BN(amounts[address]).multipliedBy(priceUsd).decimalPlaces(2),
+      })
+    );
+
+    balance.value = tokens.value.reduce(
+      (sum, { value }) => value.plus(sum),
+      new BN(0)
+    );
+  }
+});
 </script>
 
 <style lang="stylus">
@@ -11,36 +62,65 @@ const name = 'app';
   font-family "nimbus-sans"
   src url("assets/nimbus-sans.bold.otf") format("otf");
 
-sc-reset()
-    margin 0
-    padding 0
-    box-sizing border-box
-
-sc-disableScollBar()
-    ::-webkit-scrollbar
-        display: none;
-
 :root
-  font-size 18px
-  background #212121
+  --light-shadow: 0 5px 15px rgba(145, 160, 180, 0.15)
+  --dark-shadow: 0 -5px 15px rgba(145, 160, 180, 0.15)
+  --background: #e0e0e0
+  --text-color: #333
 
-*
-  sc-reset()
-  sc-disableScollBar()
-  font-family 'Raleway', sans-serif
-  outline none
-  scroll-behavior smooth
-  &::-webkit-scrollbar-track
-    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
-    background-color #555
-  &::-webkit-scrollbar
-    width 12px
-    background-color #F5F5F5
-  &::-webkit-scrollbar-thumb
-    box-shadow inset 0 0 6px rgba(0, 0, 0, .3)
-    background-color #252525
-  a
-    :active
-      color #e1c79b
-      fill #e1c79b
+.main
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+  height: 100vh
+  background: var(--background)
+  width 100vw
+  overflow: hidden
+  .address
+    color: var(--text-color)
+    text-shadow: var(--dark-shadow)
+    font-size: .9rem
+    margin-bottom: 1em
+    padding: 1em 0
+    border-radius: 1em
+    width 100vw
+    &.valid
+      color: #2ECC71
+  .value
+    color: var(--text-color)
+    text-shadow: var(--dark-shadow)
+    font-size: 1.2rem
+    padding: 1em
+
+  .tokens
+    display flex
+    flex-flow column nowrap
+    .token
+      display grid
+      align-items baseline
+      grid "name value" 1fr / 150px 150px
+      padding .5em 1em
+      margin 1em 0
+      background: var(--background)
+      box-shadow: var(--light-shadow), var(--dark-shadow)
+      text-shadow: var(--dark-shadow)
+      border-radius 10px
+      .name
+        padding-right 1em
+        text-align right
+        font-weight 900
+        font-size .8em
+        opacity .8
+      .balance
+        color #8E44AD
+
+
+body, html
+  height: 100%
+  margin: 0
+  padding: 0
+  font-family: 'Raleway', sans-serif
+  background: var(--background)
+  color: var(--text-color)
 </style>
---
+++ src/sui.js
@@ -0,0 +1,26 @@
+import { JsonRpcProvider, Connection } from '@mysten/sui.js';
+import BN from 'bignumber.js';
+
+const provider = new JsonRpcProvider(
+  new Connection({ fullnode: 'https://fullnode.mainnet.sui.io:443' })
+);
+
+export async function get_tokens(owner) {
+  const coins = await provider.getAllBalances({ owner });
+  const metas = await Promise.all(
+    coins.map(async ({ coinType, totalBalance }) => ({
+      type: coinType,
+      balance: totalBalance,
+      decimals: await provider
+        .getCoinMetadata({ coinType })
+        .then(({ decimals }) => decimals),
+    }))
+  );
+  return metas.reduce(
+    (result, { type, balance, decimals }) => ({
+      ...result,
+      [type]: new BN(balance).div(10 ** decimals).toString(),
+    }),
+    {}
+  );
+}
